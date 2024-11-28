de4a4034e0180698fd363db18b97aaa86b6b3076
Esteban Antier <esteban.antier@student-cs.fr>
2024-11-28 13:36:21 +0900

    Test for graph 5 parameters without epsilon variation

--- main.cpp
+++ main.cpp
@@ -39,16 +39,18 @@ const double Cp = 475; // Heat capacity in J/(kg.K)
 const double T_ext = 300; // Outside temperature in K
 const double T_0 = 1100; // Initial temperature in K
 
-const double hconv = 730; // Convective heat coeff
+//const double hconv = 730; // Convective heat coeff
 //const double hnb = 15000; // Nucleation boiling heat transfer
-const double hfb = 100; // Heat transfer with oil vapor
+//const double hfb = 100; // Heat transfer with oil vapor
 
 double hnb_list[3] = { 10000 , 15000 , 20000 };
+double hfb_list[3] = { 50 , 100 , 150 };
+double hconv_list[3] = { 365 , 730 , 1095 };
 double Lnb_list[3] = { 0.005 , 0.01 , 0.015 };
 double Vconv_list[3] = { 0.02 , 0.03 , 0.04 }; // Speed of the nucleation bubbling layer displacement
 
-const double stand_dev = 0.005; // Standard deviation of the gaussian distribution used
-const int num_loops = 100;
+//const double stand_dev = 0.005; // Standard deviation of the gaussian distribution used
+//const int num_loops = 100;
 
 //---------------------------------------------
 
@@ -80,16 +82,16 @@ static double f_function(double x, double y, double z) {
 static double h(double x, double y, double z, std::vector<double>& params) {
     // Params vector stores information for planes at the boundary
     // params[0] -> n1x, params[1] -> n1y, params[2] -> d1, params[3] -> n2x, params[4] -> n2y, params[5] -> d2
-    // params[6] -> hnb
+    // params[6] -> hnb, params[7] -> hfb, params[8] -> hconv
     double above1 = params[0] * x + params[1] * y + z - params[2];
     double above2 = params[3] * x + params[4] * y + z - params[5];
     // If above1>0, we are "above" plane 1, same for above2>0 and plane 2
 
     if (above1 <= 0 && above2 <= 0) {
-        return hconv;
+        return params[8];
     }
     else if (above1 > 0 && above2 > 0) {
-        return hfb;
+        return params[7];
     }
     else {
         return params[6];
@@ -264,7 +266,7 @@ int main() {
     std::random_device rd{};
     std::mt19937 gen{ rd() };
 
-    std::normal_distribution<double> gauss{ 0, stand_dev };
+    /*std::normal_distribution<double> gauss{ 0, stand_dev };
     auto random_float = [&gen, &gauss] {return gauss(gen); };
     std::vector<std::vector<double>> random_process(num_loops, std::vector<double>(n_time));
     for (int i = 0; i < n_time; i++) {
@@ -272,7 +274,7 @@ int main() {
             random_process[j][i] = random_float();
         }
     }
-    store_2d_vector_in_file("results/random_process", random_process);
+    store_2d_vector_in_file("results/random_process", random_process);*/
 
     // Read mesh in file, prepare result writer
     Mesh Reader;
@@ -317,88 +319,91 @@ int main() {
     CSRMatrix C_CSR(C);
 
 
-    for (int i_h = 0; i_h < 3; i_h++) {
-        for (int i_L = 0; i_L < 3; i_L++) {
-            for (int i_Ldot = 0; i_Ldot < 3; i_Ldot++) {
-                for (int i_loop = 0; i_loop < num_loops; i_loop++) {
-                    std::string path_storage = "results/loop_" + to_string(i_h * 9 * num_loops + i_L * 3 * num_loops + i_Ldot * num_loops + i_loop + 1) + "/";
-                    std::filesystem::create_directory(path_storage);
+    for (int i_hnb = 0; i_hnb < 3; i_hnb++) {
+        for (int i_hfb = 0; i_hfb < 3; i_hfb++) {
+            for (int i_hconv = 0; i_hconv < 3; i_hconv++) {
+                for (int i_L = 0; i_L < 3; i_L++) {
+                    for (int i_Vconv = 0; i_Vconv < 3; i_Vconv++) {
+                        std::string path_storage = "results/loop_" + to_string(i_hnb * 81 + i_hfb * 27 + i_hconv * 9 + i_L * 3 + i_Vconv + 1) + "/";
+                        std::filesystem::create_directory(path_storage);
 
-                    // We set the parameters for now:
-                    double hnb = hnb_list[i_h];
-                    double Lnb = Lnb_list[i_L];
-                    double Vconv = Vconv_list[i_Ldot];
+                        // We set the parameters for now:
+                        double hnb = hnb_list[i_hnb];
+                        double hfb = hfb_list[i_hfb];
+                        double hconv = hconv_list[i_hconv];
+                        double Lnb = Lnb_list[i_L];
+                        double Vconv = Vconv_list[i_Vconv];
 
-                    // Vectors for solving
-                    DataVector F(n_dof);
-                    DataVector F_previous(n_dof);
-                    DataVector dP(n_dof);
-                    DataVector dP_previous(n_dof);
+                        // Vectors for solving
+                        DataVector F(n_dof);
+                        DataVector F_previous(n_dof);
+                        DataVector dP(n_dof);
+                        DataVector dP_previous(n_dof);
 
-                    std::vector<double> T_C(n_time + 1); // Storing the values of temperature at center
-                    //std::cout << Reader.Nodes[3381][0] << " " << Reader.Nodes[3381][1] << " " << Reader.Nodes[3381][2] << std::endl;
-                    // for 10x10x40 mesh Center of sample is node 3381
+                        std::vector<double> T_C(n_time + 1); // Storing the values of temperature at center
+                        //std::cout << Reader.Nodes[3381][0] << " " << Reader.Nodes[3381][1] << " " << Reader.Nodes[3381][2] << std::endl;
+                        // for 10x10x40 mesh Center of sample is node 3381
 
-                    // Build force vector for timestep 0
-                    MBuild.build_vector(Reader, F, IntegrateF, f_function);
-                    // Put F into previous F vector
-                    std::copy(F.begin(), F.end(), F_previous.begin());
-                    // Reset F vector before next time step
-                    std::fill(F.begin(), F.end(), 0.0);
-
-
-                    // First, we calculate the initial condition at t=0
-                    MBuild.build_initial_T(Reader, dP_previous, T0);
-                    T_C[0] = dP_previous[3381];
-                    store_1d_vector_in_binary_file(path_storage + "T0", dP_previous);
-
-                    std::vector<double> Lconv_vector(n_time + 1); // Storage for the realizations of random
-
-                    for (int i = 0; i < n_time; i++) {
-                        double t = (i + 1) * dt;
-                        double gaussian_number = random_process[i_loop][i];
-                        Lconv_vector[i + 1] = Lconv_vector[i] + dt * Vconv + gaussian_number;
-
-                        cout << "Loop number n=" << i_loop + 1 << "/" << num_loops << ", Timestep t = " << t << endl;
-                        cout << "Params : hnb=" << hnb << " Lnb=" << Lnb << " Vconv=" << Vconv << endl;
-                        cout << "Random number : " << gaussian_number << endl;
-
-                        std::cout << "Build force vector" << std::endl;
+                        // Build force vector for timestep 0
                         MBuild.build_vector(Reader, F, IntegrateF, f_function);
-
-                        // Build A_matrix and b_vector
-                        COOMatrix A_matrix(C_CSR + dt / 2 * K_CSR);
-                        DataVector b_vector = (C_CSR - dt / 2 * K_CSR) * dP_previous + dt / 2 * (F + F_previous);
-
-
-                        // Robin BC
-                        std::cout << "Robin BC " << std::endl;
-                        // Params vector stores information for planes at the boundary
-                        // params[0] -> n1x, params[1] -> n1y, params[2] -> d1, params[3] -> n2x, params[4] -> n2y, params[5] -> d2, params[6] -> hnb
-                        std::vector<double> params_robin = { 0, 0, Lconv_vector[i + 1], 0, 0 , Lconv_vector[i + 1] + Lnb, hnb };
-                        Boundary_Vector_Integral3D Neumann_integrator;
-                        Boundary_Matrix_Integral3D Robin_integrator;
-                        MBuild.robin_BC(Reader, b_vector, A_matrix, Neumann_integrator, Robin_integrator, \
-                            Robin_vector, Robin_matrix, all_surf, params_robin);
-
-                        std::cout << "Solving system" << std::endl;
-                        // First convert from COO to CSR for solver
-                        CSRMatrix A_CSR(A_matrix);
-                        // Solve system with iterative PCCG solver
-                        dP = Solver.PCCG(A_CSR, b_vector, dP_previous);
-
-
-                        // Store solution
-                        store_1d_vector_in_binary_file(path_storage + "T" + std::to_string(i + 1), dP);
-                        T_C[i + 1] = dP[3381];
-
-
-                        dP_previous.swap(dP);
-                        F_previous.swap(F);
+                        // Put F into previous F vector
+                        std::copy(F.begin(), F.end(), F_previous.begin());
+                        // Reset F vector before next time step
                         std::fill(F.begin(), F.end(), 0.0);
+
+
+                        // First, we calculate the initial condition at t=0
+                        MBuild.build_initial_T(Reader, dP_previous, T0);
+                        T_C[0] = dP_previous[3381];
+                        store_1d_vector_in_binary_file(path_storage + "T0", dP_previous);
+
+                        std::vector<double> Lconv_vector(n_time + 1); // Storage for the realizations of random
+
+                        for (int i = 0; i < n_time; i++) {
+                            double t = (i + 1) * dt;
+                            //double gaussian_number = random_process[i_loop][i];
+                            Lconv_vector[i + 1] = Lconv_vector[i] + dt * Vconv;
+
+                            cout << "Loop number n=" + to_string(i_hnb * 81 + i_hfb * 27 + i_hconv * 9 + i_L * 3 + i_Vconv + 1) + "/" + std::to_string(int(pow(3,5))) << ", Timestep t = " << t << endl;
+                            cout << "Params : hnb=" << hnb << " hfb=" << hfb << " hconv = " << hconv << " Lnb = " << Lnb << " Vconv = " << Vconv << endl;
+
+                            std::cout << "Build force vector" << std::endl;
+                            MBuild.build_vector(Reader, F, IntegrateF, f_function);
+
+                            // Build A_matrix and b_vector
+                            COOMatrix A_matrix(C_CSR + dt / 2 * K_CSR);
+                            DataVector b_vector = (C_CSR - dt / 2 * K_CSR) * dP_previous + dt / 2 * (F + F_previous);
+
+
+                            // Robin BC
+                            std::cout << "Robin BC " << std::endl;
+                            // Params vector stores information for planes at the boundary
+                            // params[0] -> n1x, params[1] -> n1y, params[2] -> d1, params[3] -> n2x, params[4] -> n2y, params[5] -> d2, params[6] -> hnb, params[7] -> hfb, params[8] -> hconv
+                            std::vector<double> params_robin = { 0, 0, Lconv_vector[i + 1], 0, 0 , Lconv_vector[i + 1] + Lnb, hnb, hfb, hconv };
+                            Boundary_Vector_Integral3D Neumann_integrator;
+                            Boundary_Matrix_Integral3D Robin_integrator;
+                            MBuild.robin_BC(Reader, b_vector, A_matrix, Neumann_integrator, Robin_integrator, \
+                                Robin_vector, Robin_matrix, all_surf, params_robin);
+
+                            std::cout << "Solving system" << std::endl;
+                            // First convert from COO to CSR for solver
+                            CSRMatrix A_CSR(A_matrix);
+                            // Solve system with iterative PCCG solver
+                            dP = Solver.PCCG(A_CSR, b_vector, dP_previous);
+
+
+                            // Store solution
+                            store_1d_vector_in_binary_file(path_storage + "T" + std::to_string(i + 1), dP);
+                            T_C[i + 1] = dP[3381];
+
+
+                            dP_previous.swap(dP);
+                            F_previous.swap(F);
+                            std::fill(F.begin(), F.end(), 0.0);
+                        }
+                        store_1d_vector_in_file(path_storage + "Lconv", Lconv_vector);
+                        store_1d_vector_in_file(path_storage + "T_C", T_C);
                     }
-                    store_1d_vector_in_file(path_storage + "Lconv", Lconv_vector);
-                    store_1d_vector_in_file(path_storage + "T_C", T_C);
                 }
             }
         }
